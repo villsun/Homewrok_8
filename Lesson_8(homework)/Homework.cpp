3756a131caa9f916922207421dee156aaf475c72
villsun <lonukkola@gmail.com>
2024-02-21 23:13:11 +0100

    Add user menu

--- Lesson_8(homework)/Homework.cpp
+++ Lesson_8(homework)/Homework.cpp
@@ -147,19 +147,74 @@ int main()
 
 	const int SIZE = 10;
 	Student students[10];
+	int counter = 5;
 
-	strcpy(students[0].full_name, "kola");
-	strcpy(students[1].full_name, "hopa");
+	strcpy(students[0].full_name, "Loniuk Mykola Mykhailovych");
+	strcpy(students[1].full_name, "Roma Pavlyuk Olegovich");
+	strcpy(students[2].full_name, "Ilya Mazar Schumacherovich");
+	strcpy(students[3].full_name, "Artem Levchuk Hvorovych");
+	strcpy(students[4].full_name, "Sasha Dovych Petrovych");
 
-	Fill_Students_Rating(students, 2, SIZE);
+	Fill_Students_Rating(students, counter, 10);
 
-	Show_Students(students, 2, SIZE);
+	int user_input;
+	char str_student[100];
+	int user_input_rating;
+	int user_input_index_rating;
+	do
+	{
+		do
+		{
+			cout << "Enter what do you want to know:\n1 - Add student\n2 - Add rating student\n3 - Show students\n4 - Show rating student\n5 - Show debtor\n";
+			cin >> user_input;
+			if (user_input < -1 || user_input>5)
+			{
+				cout << "Error!\n";
+			}
+		} while (user_input < -1 || user_input>5);
+		if (user_input == -1)
+		{
+			break;
+		}
+		switch (user_input)
+		{
+		case 1:
+			cin.ignore();
+			if (counter > 10)
+			{
+				cout << "Many students!\nStop!\n";
+				break;
+			}
+			cin.getline(str_student, 100);
+			Add_Student(students, str_student, counter);
+			counter++;
 
-	Show_Debtor(students, 2, SIZE);
-
-
-
-
+			break;
+		case 2:
+			do
+			{
+				cout << "Enter index student:\n";
+				cin >> user_input_rating;
+			} while (user_input_rating < counter - 1 || user_input>10);
+			Add_Rating_Student(students, user_input_rating - 1, 10);
+			break;
+		case 3:
+			Show_Students(students, counter, 10);
+			break;
+		case 4:
+			do
+			{
+				cout << "Enter index student of rating:\n";
+				cin >> user_input_index_rating;
+			} while (user_input < 0 || user_input>counter - 1);
+			Show_Rating(students, user_input_index_rating - 1, 10);
+			break;
+		case 5:
+			Show_Debtor(students, counter, 10);
+		default:
+			break;
+		}
+	} while (user_input != -1);
 
 	return 0;
 }
